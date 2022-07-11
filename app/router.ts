a72e68f56d35948dbbfc11a14b0e025dba7802cb
v_raggazhao <965061947@qq.com>
2022-07-11 15:46:22 +0800

    feat: 测试commit

--- app/router.ts
+++ app/router.ts
@@ -3,5 +3,6 @@ import { Application } from 'egg';
 export default (app: Application) => {
     const { controller, router } = app;
 
+    // 测试下commit
     router.get('/*', controller.home.index);
 };
