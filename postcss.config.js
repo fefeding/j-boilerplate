72708029536e2f79742d45e38f01c9288fb5aca7
funkyLover <ahui-lin@qq.com>
2022-07-15 14:17:02 +0800

    fix: postcss config;

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,10 @@ module.exports = {
         pxtorem({
             rootValue: 20,
             propList: ['*'],
+            exclude: file => {
+                // @NOTE: 只有移动端页面需要px2rem, 这里判断可能不准确, 请自行修改
+                return !file.includes('mobile');
+            },
         }),
     ],
 };
